b0b6e4fdc1adfe8be8626272d3bd6f934833e968
Md. Saiful Haque Saif <saifulhaquesaifpc@gmail.com>
2025-07-05 14:34:50 +0600

    Added

---
+++ 026_function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b);      //function prototype
+int sum(int a, int b){      //function definition
+    int c = a+b;
+    return c;
+}
+
+int main(){
+    
+    int a, b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << "The sum of " << a << " and " << b << " is: " << sum(a,b) << endl;
+
+    return 0;
+}
