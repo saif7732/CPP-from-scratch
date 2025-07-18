4beb9772383087f149b81409108eb51b055c852b
Md. Saiful Haque Saif <saifulhaquesaifpc@gmail.com>
2025-07-18 15:43:32 +0600

    added

---
+++ OOP/007_object_arr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class employee{
+    string name;
+    int id;
+    public:
+        void setdata(){
+            cout << "Enter the name of employee: ";
+            cin >> name;
+            cout << "Enter the id of employee: ";
+            cin >> id;
+        }
+        void getdata(){
+            cout << "The name of employee is: " << name << endl;
+            cout << "The id of employee is: " << id << endl;
+        }
+};
+
+int main(){
+    
+    // employee saif, naim, shawon;
+
+    // saif.setdata();
+    // saif.getdata();
+
+    employee cse[5];
+
+    for(int i = 0; i<5; i++){
+        cse[i].setdata();
+    }
+    for(int i = 0; i<5; i++){
+        cse[i].getdata();
+    }
+
+    return 0;
+}
